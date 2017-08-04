2354a0e6098967d70bbd9c5d355ce05a9d933bcd
AndroidLollipop <liusiyongrd@gmail.com>
2017-08-04 19:05:14 +0800

    Fixed image

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -14,7 +14,6 @@ import {
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
-import AutoHeightImage from 'react-native-auto-height-image';
 
 import {
   Cell,
@@ -37,8 +36,13 @@ const win = Dimensions.get('window');
 export default class LinksScreen extends React.Component {
   constructor(props){
     super(props);
+    this.state={widthArray:[],heightArray:[]}
     this.gotoExpandedView = this.gotoExpandedView.bind(this)
     this.mapToView = this.mapToView.bind(this)
+    this.guardAccessHeight = this.guardAccessHeight.bind(this)
+    this.guardAccessWidth = this.guardAccessWidth.bind(this)
+    this.guardSetHeight = this.guardSetHeight.bind(this)
+    this.guardSetWidth = this.guardSetWidth.bind(this)
   }
 
   static route = {
@@ -61,60 +65,108 @@ export default class LinksScreen extends React.Component {
     return false
   }
 
+  guardAccessHeight(i){
+    try{
+      return this.state.heightArray[i]
+    }
+    catch(err){
+      return 1
+    }
+  }
+
+  guardAccessWidth(i){
+    try{
+      return this.state.widthArray[i]
+    }
+    catch(err){
+      return 1
+    }
+  }
+
+  guardSetHeight(x, i){
+    if (x == this.state.heightArray[i]) return
+    //DO NOT REMOVE THE ABOVE LINE. IF YOU DO, THE APP WILL FREEZE EVERY TIME YOU ATTEMPT TO OPEN GOOD BEHAVIOURS.
+    var hsliced = this.state.heightArray.slice()
+    while (hsliced.length <= i){
+      hsliced.push(1)
+    }
+    hsliced[i] = x
+    this.setState({heightArray: hsliced})
+    console.log(hsliced.length)
+  }
+
+  guardSetWidth(x, i){
+    if (x == this.state.widthArray[i]) return
+    //DO NOT REMOVE THE ABOVE LINE. IF YOU DO, THE APP WILL FREEZE EVERY TIME YOU ATTEMPT TO OPEN GOOD BEHAVIOURS.
+    var wsliced = this.state.widthArray.slice()
+    while (wsliced.length <= i){
+      wsliced.push(1)
+    }
+    wsliced[i] = x
+    this.setState({widthArray: wsliced})
+    console.log(wsliced.length)
+  }
+
   gotoExpandedView(x){
     GLOBAL.expandedViewData = x
     this.props.navigator.push(Router.getRoute("expandedView"))
   }
-  
+
   mapToView(x, i){
-    // Image.getSize(x[5], (width, height) => {this.setState({width, height})});
+    var imwidth = 50;
+    var imheight = 50;
+    Image.getSize(x[5], (width, height) => {this.guardSetWidth(width, i); this.guardSetHeight(height, i)});
     // <Image style={{width: win.width, height: win.width / this.state.width * this.state.height, marginTop: 10, marginBottom: 10}} source={{uri: x[5]}}/>
     if (this.isLong(x[2])) {
       return (
-        <TouchableHighlight key={i} onPress={() => this.gotoExpandedView(x)}>
-          <View style={styles.container}>
-            <View style={styles.spaceContainer}>
-              <Text style={styles.spaceRow}>{}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.nameRow}>{x[0]}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.dateRow}>{x[3]}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.teacherRow}>{x[1]}</Text>
-            </View>
-
-            <View style={styles.container}>
-              <Text style={styles.descriptionRow}>{this.truncText(x[2])}</Text>
-              <Text style={styles.viewFullRow}>{"Tap to view full description"}</Text>
-            </View>
+        <View key={i}>
+          <View style={styles.spaceContainer}>
+            <Text style={styles.spaceRow}>{}</Text>
           </View>
-        </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.gotoExpandedView(x)}>
+            <View style={styles.container}>
+              <View style={styles.container}>
+                <Text style={styles.nameRow}>{x[0]}</Text>
+              </View>
+              <View style={styles.container}>
+                <Text style={styles.dateRow}>{x[3]}</Text>
+              </View>
+              <View style={styles.container}>
+                <Text style={styles.teacherRow}>{x[1]}</Text>
+              </View>
+              <Image style={{width: win.width, height: win.width / this.guardAccessWidth(i) * this.guardAccessHeight(i), marginTop: 10, marginBottom: 10}} source={{uri: x[5]}}/>
+              <View style={styles.container}>
+                <Text style={styles.descriptionRow}>{this.truncText(x[2])}</Text>
+                <Text style={styles.viewFullRow}>{"Tap to view full description"}</Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+        </View>
       );
     } else {
       return (
-        <TouchableHighlight key={i} onPress={() => this.gotoExpandedView(x)}>
-          <View style={styles.container}>
-            <View style={styles.spaceContainer}>
-              <Text style={styles.spaceRow}>{}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.nameRow}>{x[0]}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.dateRow}>{x[3]}</Text>
-            </View>
-            <View style={styles.container}>
-              <Text style={styles.teacherRow}>{x[1]}</Text>
-            </View>
-
-            <View style={styles.container}>
-              <Text style={styles.descriptionRow}>{this.truncText(x[2])}</Text>
-            </View>
+        <View key={i}>
+          <View style={styles.spaceContainer}>
+            <Text style={styles.spaceRow}>{}</Text>
           </View>
-        </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.gotoExpandedView(x)}>
+            <View style={styles.container}>
+              <View style={styles.container}>
+                <Text style={styles.nameRow}>{x[0]}</Text>
+              </View>
+              <View style={styles.container}>
+                <Text style={styles.dateRow}>{x[3]}</Text>
+              </View>
+              <View style={styles.container}>
+                <Text style={styles.teacherRow}>{x[1]}</Text>
+              </View>
+              <Image style={{width: win.width, height: win.width / this.guardAccessWidth(i) * this.guardAccessHeight(i), marginTop: 10, marginBottom: 10}} source={{uri: x[5]}}/>
+              <View style={styles.container}>
+                <Text style={styles.descriptionRow}>{this.truncText(x[2])}</Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+        </View>
       );
     }
   }
